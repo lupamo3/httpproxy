d55136afa62f8314328422727e68bdc4b82ce7da
Norbert Lupamo <91078038+lupamo3@users.noreply.github.com>
2023-01-23 12:19:03 +0300

    changes to the status route processing requests

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+import requests
+import uuid
 from datetime import datetime
 from flask import Flask, request, jsonify
 from jose import jwt
 from jose.backends import RSAKey
+from datetime import datetime
+
+requests_processed = 0
+start_time = datetime.now()
+
 
 app = Flask(__name__)
 secret = "a9ddbcaba8c0ac1a0a812dc0c2f08514b23f2db0a68343cb8199ebb38a6d91e4ebfb378e22ad39c2d01d0b4ec9c34aa91056862ddace3fbbd6852ee60c36acbf"
 
+def generate_nonce():
+    return str(uuid.uuid4())
+
 @app.route("/", methods=["POST"])
 def proxy():
+    global requests_processed
+    requests_processed += 1
     # Get the current timestamp
     iat = int(datetime.utcnow().timestamp())
     # Generate a unique nonce
@@ -29,7 +41,10 @@ def proxy():
 @app.route("/status")
 def status():
     # Return the time from start and the number of requests processed
-    return jsonify({"time_from_start": time.time() - start_time, "requests_processed": requests_processed})
+    current_time = datetime.now()
+    uptime = current_time - start_time
+    return jsonify(requests_processed=requests_processed, uptime=str(uptime))
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
